33ea3925ec69ef0ab630dffb94461b81d9d1f274
TradeSis Helio <helio@tradesis.com.br>
2024-04-10 10:30:11 -0300

    Update modalFornecedor_Alterar.php

--- geral/modalFornecedor_Alterar.php
+++ geral/modalFornecedor_Alterar.php
@@ -68,6 +68,9 @@ e gás natural">7 - Estrangeira - Adquirida no mercado interno</option>
                             </div>
                     </div><!--body-->
                     <div class="modal-footer">
+                        <div class="col align-self-start pl-0">
+                            <button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#atualizaFornecedorModal' data-id="idFornecedorAtualiza">Atualizar fiscal</button>
+                        </div>
                         <button type="submit" class="btn btn-success">Salvar</button>
                     </div>
                     </form>
